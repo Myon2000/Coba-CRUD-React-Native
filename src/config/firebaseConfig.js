44d118dd7ad7fd46132c379990c14f10bf5d79f3
Sogol <oktavianramadhani25@gmail.com>
2024-11-27 20:22:13 +0700

    hapus api key

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmHUPxbMVaoQZJW31Z2bFYlE2eJRyDFBc",
-  authDomain: "react-navite-funcpro.firebaseapp.com",
-  projectId: "react-navite-funcpro",
-  storageBucket: "react-navite-funcpro.firebasestorage.app",
-  messagingSenderId: "344988874648",
-  appId: "1:344988874648:web:ea80edf09ed2daec241ded",
-  measurementId: "G-EYPLTYLY51"
+
 };
 
 const app = initializeApp(firebaseConfig);
